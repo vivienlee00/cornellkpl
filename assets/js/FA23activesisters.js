052d00c5e3184a285ad2d0202a1757db87f26c47
cl962 <cl962@cornell.edu>
2023-09-01 21:54:35 -0400

    update active sisters

---
+++ assets/js/FA23activesisters.js
@@ -0,0 +1,511 @@
+data = [
+  {
+    "big": "Kelly *sappho* Foo",
+    "description": "spending $5 on kombucha at Nasties",
+    "ethnicity": "Chinese",
+    "first_name": "Grace",
+    "hometown": "Buffalo Grove, IL",
+    "last_name": "Fan",
+    "line_number": 190,
+    "little": "Vanessa *sylvié* Tam",
+    "major": "Computer Science",
+    "nickname": "vendela",
+    "semester": "Spring 2021",
+    "year": 2024
+  },
+  {
+    "big": "Nicole *SYRE* Hu",
+    "description": "falling asleep in the cocktail lounge",
+    "ethnicity": "Korean",
+    "first_name": "Christine",
+    "hometown": "San Diego, CA",
+    "last_name": "Cho",
+    "line_number": 198,
+    "little": "n/a",
+    "major": "Statistical Science",
+    "nickname": "enoki",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Ling *unix* Cao",
+    "description": "buying/eating ice cream or froyo 🥰",
+    "ethnicity": "Korean",
+    "first_name": "Ga Yoon",
+    "hometown": "Seoul, South Korea",
+    "last_name": "Choi",
+    "line_number": 199,
+    "little": "Pin *novalunne* Assakul",
+    "major": "Digital Design",
+    "nickname": "mertsanie",
+    "semester": "Fall 2021",
+    "year": 2023
+  },
+  {
+    "big": "Michelle *Iolite* Li",
+    "description": "in distress",
+    "ethnicity": "Chinese",
+    "first_name": "Sophia",
+    "hometown": "Princeton, NJ",
+    "last_name": "Teng",
+    "line_number": 200,
+    "little": "n/a",
+    "major": "Mechanical Engineering",
+    "nickname": "Kumo",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Youna *Aqua Vitae* Choi",
+    "description": "stalking you on Find My",
+    "ethnicity": "Korean",
+    "first_name": "Stephanie",
+    "hometown": "Seoul, South Korea",
+    "last_name": "Jung",
+    "line_number": 201,
+    "little": "Jessica *rhôa* Han",
+    "major": "Hotel Administration",
+    "nickname": "VARA",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Amanda *Indica* Wang",
+    "description": "napping in duffield",
+    "ethnicity": "Chinese",
+    "first_name": "Jessica",
+    "hometown": "Santa Clara, CA",
+    "last_name": "Kuang",
+    "line_number": 202,
+    "little": "n/a",
+    "major": "Information Science",
+    "nickname": "Iberis",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Ziqi *Imara* Guo",
+    "description": "in my room taking a nap",
+    "ethnicity": "Korean",
+    "first_name": "Denise",
+    "hometown": "Fort Lee, NJ",
+    "last_name": "Park",
+    "line_number": 203,
+    "little": "n/a",
+    "major": "Animal Science",
+    "nickname": "Asclepia",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Tong *Factor!al* Li",
+    "description": "chilling in my room watching YouTube",
+    "ethnicity": "Korean",
+    "first_name": "Elisa",
+    "hometown": "Atlanta, GA",
+    "last_name": "Kim",
+    "line_number": 204,
+    "little": "n/a",
+    "major": "Animal Science",
+    "nickname": "audax",
+    "semester": "Fall 2021",
+    "year": 2024
+  },
+  {
+    "big": "Grace *Bluet* Tran",
+    "description": "taking a study break",
+    "ethnicity": "Vietnamese",
+    "first_name": "Monica",
+    "hometown": "Omaha, NE",
+    "last_name": "Nguyenduy",
+    "line_number": 206,
+    "little": "n/a",
+    "major": "Information Science, Systems, and Technology",
+    "nickname": "yūgen",
+    "semester": "Spring 2022",
+    "year": 2025
+  },
+  {
+    "big": "Jennifer *mellis* Guo",
+    "description": "studying in duff or psb",
+    "ethnicity": "Chinese",
+    "first_name": "Sara",
+    "hometown": "Roslyn, NY",
+    "last_name": "Lok",
+    "line_number": 207,
+    "little": "n/a",
+    "major": "Biomedical Engineering",
+    "nickname": "Kodak",
+    "semester": "Spring 2022",
+    "year": 2025
+  },
+  {
+    "big": "Rachel *imugi* Kwon",
+    "description": "pulling an allnighter in duff with an energy drink",
+    "ethnicity": "Korean",
+    "first_name": "Ashley",
+    "hometown": "Palisades Park, NJ",
+    "last_name": "An",
+    "line_number": 208,
+    "little": "n/a",
+    "major": "Materials Science and Engineering",
+    "nickname": "yokan",
+    "semester": "Spring 2022",
+    "year": 2025
+  },
+  {
+    "big": "Grace *Bluet* Tran",
+    "description": "on the fifth floor of the Johnson Museum",
+    "ethnicity": "Korean",
+    "first_name": "Leah",
+    "hometown": "Norwood, NJ",
+    "last_name": "Han",
+    "line_number": 209,
+    "little": "n/a",
+    "major": "Art History and College Scholar Program",
+    "nickname": "cloudbreak",
+    "semester": "Spring 2022",
+    "year": 2025
+  },
+  {
+    "big": "Juliana *trevi* Kim",
+    "description": "with airpods in listening to music",
+    "ethnicity": "Chinese",
+    "first_name": "Chelsea",
+    "hometown": "Seaford, NY",
+    "last_name": "Lin",
+    "line_number": 210,
+    "little": "n/a",
+    "major": "Applied Economics and Management",
+    "nickname": "kyem",
+    "semester": "Spring 2022",
+    "year": 2025
+  },
+  {
+    "big": "Lina *Maélle* Cheung",
+    "description": "at bus stop bagels about to get my 5th latte of the week",
+    "ethnicity": "Chinese",
+    "first_name": "Angie",
+    "hometown": "Los Angeles, CA",
+    "last_name": "Lee",
+    "line_number": 211,
+    "little": "n/a",
+    "major": "Human Biology, Health and Society",
+    "nickname": "Mitotique",
+    "semester": "Fall 2022",
+    "year": 2025
+  },
+  {
+    "big": "Ga Yoon *mertsanie* Choi",
+    "description": "waiting in line at Bus Stop Bagels",
+    "ethnicity": "Thai",
+    "first_name": "Pin",
+    "hometown": "Bangkok, Thailand",
+    "last_name": "Assakul",
+    "line_number": 212,
+    "little": "n/a",
+    "major": "Human Development",
+    "nickname": "novalunne",
+    "semester": "Fall 2022",
+    "year": 2025
+  },
+  {
+    "big": "Lucy *Dalmore* Huang",
+    "description": "taking a nap in Rhodes",
+    "ethnicity": "Taiwanese/Cantonese",
+    "first_name": "Jasmine",
+    "hometown": "Pittsburgh, PA",
+    "last_name": "Eng",
+    "line_number": 213,
+    "little": "n/a",
+    "major": "Operations Research and Information Engineering",
+    "nickname": "Clairette",
+    "semester": "Fall 2022",
+    "year": 2024
+  },
+  {
+    "big": "Jasmine *mirai* Yu",
+    "description": "looking at your Spotify friend activity",
+    "ethnicity": "Taiwanese",
+    "first_name": "Jackie",
+    "hometown": "San Diego, CA",
+    "last_name": "Wu",
+    "line_number": 214,
+    "little": "n/a",
+    "major": "Economics, Law and Society",
+    "nickname": "kahana",
+    "semester": "Fall 2022",
+    "year": 2025
+  },
+  {
+    "big": "Arden *ad astra* Lee",
+    "description": "with my puppies",
+    "ethnicity": "Korean",
+    "first_name": "Lyss",
+    "hometown": "Jericho, NY",
+    "last_name": "Paek",
+    "line_number": 215,
+    "little": "n/a",
+    "major": "Communication",
+    "nickname": "HESPEROS",
+    "semester": "Fall 2022",
+    "year": 2025
+  },
+  {
+    "big": "Xue *kopari* Shao",
+    "description": "cheffin it up in the kitchen",
+    "ethnicity": "Chinese",
+    "first_name": "Kelly",
+    "hometown": "Williston Park, NY",
+    "last_name": "Feng",
+    "line_number": 216,
+    "little": "n/a",
+    "major": "Applied Economics and Management",
+    "nickname": "Sutera",
+    "semester": "Spring 2023",
+    "year": 2026
+  },
+  {
+    "big": "Tiffany *claire voie* Chen",
+    "description": "scrolling on insta reels 🤭",
+    "ethnicity": "Chinese",
+    "first_name": "Emily",
+    "hometown": "Syosset, NY",
+    "last_name": "Chen",
+    "line_number": 217,
+    "little": "n/a",
+    "major": "Biological Engineering",
+    "nickname": "evanthia",
+    "semester": "Spring 2023",
+    "year": 2025
+  },
+  {
+    "big": "Grace *vendela* Fan",
+    "description": "grabbing a medium less ice lychee black tea w/ herbal jelly at kung fu tea 😋",
+    "ethnicity": "Chinese",
+    "first_name": "Vanessa",
+    "hometown": "New York, NY",
+    "last_name": "Tam",
+    "line_number": 218,
+    "little": "n/a",
+    "major": "Operations Research & Information Engineering",
+    "nickname": "sylvié",
+    "semester": "Spring 2023",
+    "year": 2026
+  },
+  {
+    "big": "Stephanie *VARA* Jung",
+    "description": "waiting in line for ice cream at the dining halls",
+    "ethnicity": "Korean",
+    "first_name": "Jessica",
+    "hometown": "Englewood Cliffs, NJ",
+    "last_name": "Han",
+    "line_number": 219,
+    "little": "n/a",
+    "major": "Human Biology, Health, and Society",
+    "nickname": "rhôa",
+    "semester": "Spring 2023",
+    "year": 2026
+  },
+  {
+    "big": "Daphne *averic* Zhu",
+    "description": "drinking a matcha latte with oat milk",
+    "ethnicity": "Chinese",
+    "first_name": "Venus",
+    "hometown": "Minneapolis, MN",
+    "last_name": "Zheng",
+    "line_number": 220,
+    "little": "n/a",
+    "major": "Hotel Administration",
+    "nickname": "remaic",
+    "semester": "Spring 2023",
+    "year": 2026
+  }
+]
+
+function openPage(pageName, elmnt, color) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].style.backgroundColor = "";
+    tablinks[i].style.color = 'white';
+  }
+  document.getElementById(pageName).style.display = "block";
+  elmnt.style.backgroundColor = color;
+  elmnt.style.color = '#420000';
+}
+
+function displayClass(clss) {
+
+  if (clss == "AEta") {
+    var sem = "Spring 2021";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].semester == sem) {
+        var sister = `<div class="col-3 col-12-medium">
+                        <div class="container">
+                        <img class="image" src=images/activeroster/SP23/${data[i].line_number}.jpeg alt="" />
+                        <div class="overlay">
+                            <div class="text">
+                              <h5>#${data[i].line_number}<br>${data[i].first_name} *${data[i].nickname}* ${data[i].last_name}</h5>
+                              <p class="sisterDesc">
+                                <b><u>Major:</u></b> ${data[i].major} / ${data[i].year}<br>
+                                <b><u>Ethnicity:</u></b> ${data[i].ethnicity}<br>
+                                <b><u>Hometown:</u></b> ${data[i].hometown}<br>
+                                <b><u>Big:</u></b> ${data[i].big}<br>
+                                <b><u>Little(s):</u></b> ${data[i].little}<br><br>
+                                <b><u>You will usually find me:<br></u></b> ${data[i].description}<br>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+        $("#" + clss + " .row").append(sister);
+      }
+    }
+  }
+  if (clss == "ATheta") {
+    var sem = "Fall 2021";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].semester == sem) {
+        var sister = `<div class="col-3 col-12-medium">
+                        <div class="container">
+                        <img class="image" src=images/activeroster/SP23/${data[i].line_number}.jpeg alt="" />
+                        <div class="overlay">
+                            <div class="text">
+                              <h5>#${data[i].line_number}<br>${data[i].first_name} *${data[i].nickname}* ${data[i].last_name}</h5>
+                              <p class="sisterDesc">
+                                <b><u>Major:</u></b> ${data[i].major} / ${data[i].year}<br>
+                                <b><u>Ethnicity:</u></b> ${data[i].ethnicity}<br>
+                                <b><u>Hometown:</u></b> ${data[i].hometown}<br>
+                                <b><u>Big:</u></b> ${data[i].big}<br>
+                                <b><u>Little(s):</u></b> ${data[i].little}<br><br>
+                                <b><u>You will usually find me:<br></u></b> ${data[i].description}<br>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+        $("#" + clss + " .row").append(sister);
+      }
+    }
+  }
+  if (clss == "AIota") {
+    var sem = "Spring 2022";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].semester == sem) {
+        var sister = `<div class="col-3 col-12-medium">
+                        <div class="container">
+                        <img class="image" src=images/activeroster/SP23/${data[i].line_number}.jpeg alt="" />
+                        <div class="overlay">
+                            <div class="text">
+                              <h5>#${data[i].line_number}<br>${data[i].first_name} *${data[i].nickname}* ${data[i].last_name}</h5>
+                              <p class="sisterDesc">
+                                <b><u>Major:</u></b> ${data[i].major} / ${data[i].year}<br>
+                                <b><u>Ethnicity:</u></b> ${data[i].ethnicity}<br>
+                                <b><u>Hometown:</u></b> ${data[i].hometown}<br>
+                                <b><u>Big:</u></b> ${data[i].big}<br>
+                                <b><u>Little(s):</u></b> ${data[i].little}<br><br>
+                                <b><u>You will usually find me:<br></u></b> ${data[i].description}<br>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+        $("#" + clss + " .row").append(sister);
+      }
+    }
+  }
+  if (clss == "AKappa") {
+    var sem = "Fall 2022";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].semester == sem) {
+        var sister = `<div class="col-3 col-12-medium">
+                        <div class="container">
+                        <img class="image" src=images/activeroster/SP23/${data[i].line_number}.jpeg alt="" />
+                        <div class="overlay">
+                            <div class="text">
+                              <h5>#${data[i].line_number}<br>${data[i].first_name} *${data[i].nickname}* ${data[i].last_name}</h5>
+                              <p class="sisterDesc">
+                                <b><u>Major:</u></b> ${data[i].major} / ${data[i].year}<br>
+                                <b><u>Ethnicity:</u></b> ${data[i].ethnicity}<br>
+                                <b><u>Hometown:</u></b> ${data[i].hometown}<br>
+                                <b><u>Big:</u></b> ${data[i].big}<br>
+                                <b><u>Little(s):</u></b> ${data[i].little}<br><br>
+                                <b><u>You will usually find me:<br></u></b> ${data[i].description}<br>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+        $("#" + clss + " .row").append(sister);
+      }
+    }
+  }
+  if (clss == "ALambda") {
+    var sem = "Spring 2023";
+
+    for (i = 0; i < data.length; i++) {
+      if (data[i].semester == sem) {
+        var sister = `<div class="col-3 col-12-medium">
+                        <div class="container">
+                        <img class="image" src=images/activeroster/SP23/${data[i].line_number}.jpeg alt="" />
+                        <div class="overlay">
+                            <div class="text">
+                              <h5>#${data[i].line_number}<br>${data[i].first_name} *${data[i].nickname}* ${data[i].last_name}</h5>
+                              <p class="sisterDesc">
+                                <b><u>Major:</u></b> ${data[i].major} / ${data[i].year}<br>
+                                <b><u>Ethnicity:</u></b> ${data[i].ethnicity}<br>
+                                <b><u>Hometown:</u></b> ${data[i].hometown}<br>
+                                <b><u>Big:</u></b> ${data[i].big}<br>
+                                <b><u>Little(s):</u></b> ${data[i].little}<br><br>
+                                <b><u>You will usually find me:<br></u></b> ${data[i].description}<br>
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </div>`;
+        $("#" + clss + " .row").append(sister);
+      }
+    }
+  }
+}
+
+$(document).ready(function () {
+  document.getElementById("defaultOpen").click();
+  displayClass("AEta");
+  displayClass("ATheta");
+  displayClass("AIota")
+  displayClass("AKappa")
+  displayClass("ALambda")
+});
+
+
+/*
+<div class="col-3 col-12-medium">
+                <div class="container">
+                  <img class="image" src=images/activeroster/142-min.jpg alt="" />
+                  <div class="overlay">
+                    <div class="text">
+                      <h5>#142<br>Jasmine *MIU* Ng</h5>
+                      <p class="sisterDesc">
+                        <b><u>Major:</u></b> ORIE / 2021<br>
+                        <b><u>Ethnicity:</u></b> Chinese<br>
+                        <b><u>Hometown:</u></b> Flushing, NY<br>
+                        <b><u>Big:</u></b> Min Young Erika *luciole* Kim<br>
+                        <b><u>Little(s):</u></b> Amber *laveille* Leung<br><br>
+                        <b><u>You will usually find me:<br></u></b> sleeping<br>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+*/
