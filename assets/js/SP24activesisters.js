1cca6599b32918f9f9ccb9f60dfd627a867abe5f
Venus Zheng <vz49@cornell.edu>
2024-01-26 13:19:25 -0500

    problemfixed

--- assets/js/SP24activesisters.js
+++ assets/js/SP24activesisters.js
@@ -582,6 +582,7 @@ function displayClass(clss) {
 
 $(document).ready(function () {
   document.getElementById("defaultOpen").click();
+  displayClass("AEta");
   displayClass("ATheta");
   displayClass("AIota")
   displayClass("AKappa")
